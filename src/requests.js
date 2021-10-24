4541f65c2042854b51d0c1ab2deb709f618ac1cf
James Walsh <jamesandersonwalsh@gmail.com>
2021-10-24 17:03:02 -0600

    Add request console logger

--- src/requests.js
+++ src/requests.js
@@ -5,5 +5,7 @@ export async function getFromSwapi(endpoint) {
   const request = await fetch(`${baseUrl}/${endpoint}`)
   const response = await request.json()
 
+  console.info('SWAPI Request Response', response.results, response)
+
   return response
 }
