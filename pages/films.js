0eee9f0b35ac4752ea378865e42e13258e5dd7aa
James Walsh <jamesandersonwalsh@gmail.com>
2021-10-24 16:21:59 -0600

    Fetch films in films component

--- pages/films.js
+++ pages/films.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getFromSwapi } from '../src/requests'
 
 
 const Films = (props) => {
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    async function fetchFilms() {
+      const response = await getFromSwapi('films')
+      const {results: filmsHttpResponse} = response
+
+      setFilms(filmsHttpResponse)
+    }
+
+    fetchFilms()
+  }, [])
+
   return (
-    <div>Films data will go here</div>
+    <main>
+      <ul>
+        {films.map(film => {
+          return <li key={film.episode_id}>{film.title}</li>
+        })}
+      </ul>
+    </main>
   )
 }
 
