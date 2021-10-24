8a727bd885d769a438a1c935e8b834081d3c37c5
James Walsh <jamesandersonwalsh@gmail.com>
2021-10-24 16:24:10 -0600

    Fetch vehicles in vehicles component

--- pages/vehicles.js
+++ pages/vehicles.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getFromSwapi } from '../src/requests'
 
 
 const Vehicles = (props) => {
+  const [vehicles, setVehicles] = useState([])
+
+  useEffect(() => {
+    async function fetchVehicles() {
+      const response = await getFromSwapi('vehicles')
+      const {results: vehiclesHttpResponse} = response
+
+      setVehicles(vehiclesHttpResponse)
+    }
+
+    fetchVehicles()
+  }, [])
+
+  console.log('vehicles', vehicles)
+
   return (
-    <div>Vehicles data will go here</div>
+    <main>
+      <ul>
+        {vehicles.map(vehicle => {
+          return <li key={vehicle.model}>{vehicle.name}</li>
+        })}
+      </ul>
+    </main>
   )
 }
 
