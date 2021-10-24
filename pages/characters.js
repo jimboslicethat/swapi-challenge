c4095f8f5dc486b8bc7edae5278cc0cbad8a43d9
James Walsh <jamesandersonwalsh@gmail.com>
2021-10-24 16:19:51 -0600

    Fetch people in characters component

--- pages/characters.js
+++ pages/characters.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getFromSwapi } from '../src/requests'
 
 
 const Characters = (props) => {
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    async function fetchPeople() {
+      const response = await getFromSwapi('people')
+      const {results: peopleHttpResponse} = response
+
+      setPeople(peopleHttpResponse)
+    }
+
+    fetchPeople()
+  }, [])
+
   return (
-    <div>Characters data will go here</div>
+    <main>
+      <ul>
+        {people.map(person => {
+          return <li key={person.name}>{person.name}</li>
+        })}
+      </ul>
+    </main>
   )
 }
 
